020523c500a8a535c15a14823d31ed6b9397d068
mdmadrigal <109667151+mdmadrigal@users.noreply.github.com>
2024-06-07 17:57:21 -0300

    Update criar_ec2.sh

--- ALB/criar_ec2.sh
+++ ALB/criar_ec2.sh
@@ -5,7 +5,7 @@ security_group_id=$(aws ec2 describe-security-groups --group-names "SG-Linux" --
 
 
 aws ec2 run-instances --image-id ami-02f3f602d23f1659d --count 1 --instance-type t3.micro \
---security-group-ids $security_group_id --subnet-id $subnet_id --associate-public-ip-address \
+--security-group-ids $security_group_id --subnet-id $subnet-04cbb7f7050d5e7c0 --associate-public-ip-address \
 --block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":15,"VolumeType":"gp2"}}]' \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bia-dev}]' \
 --iam-instance-profile Name=role-acesso-ssm --user-data file://user_data.sh
